66ced183affd7e963d02653990f4b05bc9991c28
jhs512 <jangka512@gmail.com>
2019-08-20 11:10:03 +0900

    ArticleDaoImpl을 제거하고 마이바티스가 대신 구현하도록 처리(마이바티스는 데이터를 DB에서 가져옴, 즉 이제 가짜 데이터는 끝)

--- src/main/java/com/sbs/starter/dao/ArticleDao.java
+++ src/main/java/com/sbs/starter/dao/ArticleDao.java
@@ -2,8 +2,11 @@ package com.sbs.starter.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.sbs.starter.dto.Article;
 
+@Mapper // 이렇게 해주면 ArticleDao의 구현체를 마이바티스가 대신 구현해준다.
 public interface ArticleDao {
 	public List<Article> getList();
 }
