e8e82db01a1788e17942ff2e91072c80f19f7d91
jhs512 <jangka512@gmail.com>
2019-08-25 12:35:29 +0900

    NeedToLogin 인터셉터, NeedToLogout 인터셉터를 적용하여, 각각의 액션마다 필요한 접근권한체크를 쉽게 처리

--- src/main/java/com/sbs/starter/config/WebMvcConfig.java
+++ src/main/java/com/sbs/starter/config/WebMvcConfig.java
@@ -14,10 +14,31 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Qualifier("beforeActionInterceptor")
 	HandlerInterceptor beforeActionInterceptor;
 
+	// needToLoginInterceptor 인터셉터 불러오기
+	@Autowired
+	@Qualifier("needToLoginInterceptor")
+	HandlerInterceptor needToLoginInterceptor;
+
+	// needToLogoutInterceptor 인터셉터 불러오기
+	@Autowired
+	@Qualifier("needToLogoutInterceptor")
+	HandlerInterceptor needToLogoutInterceptor;
+
 	// 이 함수는 인터셉터를 적용하는 역할을 합니다.
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// beforeActionInterceptor 를 모든 액션(/**)에 연결합니다. 단 /resource 로 시작하는 액션은 제외
 		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**");
+
+		// /resouce로 시작하는 URL
+		// 메인, 로그인, 로그인 처리, 가입, 가입 처리, 게시물 리스트, 게시물 상세 빼고는 모두 로그인 상태여야 접근이 가능하다.
+		registry.addInterceptor(needToLoginInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
+				.excludePathPatterns("/").excludePathPatterns("/member/login").excludePathPatterns("/member/doLogin")
+				.excludePathPatterns("/member/join").excludePathPatterns("/member/doJoin")
+				.excludePathPatterns("/article/list").excludePathPatterns("/article/detail");
+
+		// 로그인, 로그인처리, 가입, 가입 처리는 로그인 상태일 때 접근할 수 없다.
+		registry.addInterceptor(needToLogoutInterceptor).addPathPatterns("/member/login")
+				.addPathPatterns("/member/doLogin").addPathPatterns("/member/join").addPathPatterns("/member/doJoin");
 	}
 }
