2870f589e24590d43cb89b48f108d11345d0f4b1
jhs512 <jangka512@gmail.com>
2019-08-25 12:12:39 +0900

    BeforeActionInterceptor 적용하여, 매번 로그인한 회원의 정보를 DB에서 꺼내오는 작업을 간편화

---
+++ src/main/java/com/sbs/starter/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.sbs.starter.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	// beforeActionInterceptor 인터셉터 불러오기
+	@Autowired
+	@Qualifier("beforeActionInterceptor")
+	HandlerInterceptor beforeActionInterceptor;
+
+	// 이 함수는 인터셉터를 적용하는 역할을 합니다.
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// beforeActionInterceptor 를 모든 액션(/**)에 연결합니다. 단 /resource 로 시작하는 액션은 제외
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**");
+	}
+}
--- src/main/java/com/sbs/starter/controller/HomeController.java
+++ src/main/java/com/sbs/starter/controller/HomeController.java
@@ -1,6 +1,6 @@
 package com.sbs.starter.controller;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,17 +19,7 @@ public class HomeController {
 	MemberService memberService;
 
 	@RequestMapping("/home/main")
-	public String showMain(HttpSession session, Model model) {
-		long loginedMemberId = 0;
-
-		if (session.getAttribute("loginedMemberId") != null) {
-			loginedMemberId = (long) session.getAttribute("loginedMemberId");
-		}
-
-		Member loginedMember = memberService.getOne(loginedMemberId);
-		
-		model.addAttribute("loginedMember", loginedMember);
-
+	public String showMain(Model model) {
 		return "home/main";
 	}
 
